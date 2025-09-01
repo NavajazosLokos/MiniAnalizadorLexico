2f27ce11f3c2dfc403ae24a376aef679f2d4a248
NavajazosLokos <orochivenom123@gmail.com>
2025-08-31 23:49:41 -0600

    cambios a entradas del code

--- analizador_léxico_completo/principal.cpp
+++ analizador_léxico_completo/principal.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
 
     
     Lexico lexico;
-    lexico.entrada("+-+");
+    lexico.entrada("+-+//4");
     
     cout << "Resultado del Analisis Lexico" << endl << endl;
     cout << "Simbolo\t\tTipo" << endl;
